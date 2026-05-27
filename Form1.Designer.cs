4212af1b291932b5ecdddc6c8f43d54c26c1bfac
phakathithandolwethu2-cell <phakathithandolwethu2@gmail.com>
2026-05-27 11:55:58 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MobileStockCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Location = new System.Drawing.Point(117, 33);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.ReadOnly = true;
+            this.lblOutput.Size = new System.Drawing.Size(504, 22);
+            this.lblOutput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mobile Code";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(239, 74);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(320, 22);
+            this.txtCode.TabIndex = 2;
+            this.txtCode.TextChanged += new System.EventHandler(this.txtCode_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Make";
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(239, 114);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(320, 22);
+            this.txtMake.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Quantity";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(242, 163);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(316, 22);
+            this.txtQuantity.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(89, 226);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(176, 44);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(305, 226);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(157, 43);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(504, 226);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(157, 43);
+            this.btnFind.TabIndex = 9;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMake);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lblOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MobileStockCapture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCode_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblOutput_TextChanged(object sender, EventArgs e)
+        {
+            
+            lblOutput.ReadOnly = true;
+        }
+       
+        private void btnAdd_Click(object sender, EventArgs e)
+     
+        {
+           
+
+            string mobileCode = txtCode.Text;
+            string make = txtMake.Text;
+            int quantity;
+            if (int.TryParse(txtQuantity.Text, out quantity) && !string.IsNullOrEmpty(make))
+            {
+                
+                lblOutput.Text = "Record Added";
+            }
+            else
+            {
+                lblOutput.Text = "Please enter a valid quantity.";
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        { 
+            //
+            string mobileCode = txtCode.Text;
+            string make = txtMake.Text;
+            if (!string.IsNullOrEmpty(mobileCode))
+            {
+                lblOutput.Text = "Record Deleted";
+            }
+            else
+            {
+                lblOutput.Text = "Record NOT Found";
+            }
+
+        }
+        /* difference between load testing and stress testing in non-functional test design.
+         * load testing is a type of performance testing that evaluates how a system performs under expected or normal load conditions. It simulates real-world usage to determine if the system can handle the anticipated number of users, transactions, or data volume without significant performance degradation. The primary goal of load testing is to identify bottlenecks and ensure that the system can meet performance requirements under typical usage scenarios. while stress testing is a type of performance testing that evaluates how a system performs under extreme or beyond-normal load conditions. It intentionally pushes the system to its limits to identify its breaking point and how it handles high levels of stress. The primary goal of stress testing is to determine the system's robustness, stability, and ability to recover from failure when subjected to excessive load or resource constraints. In summary, load testing focuses on normal usage scenarios, while stress testing focuses on extreme conditions to assess system resilience.*/
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+
+            string mobileCode = txtCode.Text;
+            string make = txtMake.Text;
+            if (!string.IsNullOrEmpty(mobileCode))
+            {
+                lblOutput.Text = "Record Found";
+            }
+            else
+            {
+                lblOutput.Text = "Record NOT Found";
+            }
+            
+
+        }
+    }
+}
